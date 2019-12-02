b8bea40d721ebfc390c71e17aa29c93fe9ce0039
TKFM21 <tkzw21@gmail.com>
2019-12-02 16:07:08 +0900

    add update todo to todo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Route
+} from 'react-router-dom';
 import './App.css';
 import Todo from './components/Todo/Todo';
+import TodoEdit from './components/TodoEdit/TodoEdit';
 
 function App() {
     return (
-        <div className="App">
-            <Todo />
-        </div>
+        <Router>
+            <div className="App">
+                <Route path="/" exact component={Todo} />
+                <Route path="/:id" exact component={TodoEdit} />
+            </div>
+        </Router>
     );
 }
 
